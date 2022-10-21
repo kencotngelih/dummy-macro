b11f0c6e8ea4c5e003b4c7d07315e18ad8641cf7
Robby Suliantoro <robby.suliantoro@gmail.com>
2022-10-21 15:01:18 +0700

    Initial Commit

---
+++ dummy-macro/dummy_macroApp.swift
@@ -0,0 +1,17 @@
+//
+//  dummy_macroApp.swift
+//  dummy-macro
+//
+//  Created by Robby Suliantoro on 21/10/22.
+//
+
+import SwiftUI
+
+@main
+struct dummy_macroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
